e1aae58623b1654d473bbf7b18ff640346efcab5
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2026-04-04 20:13:26 +0200

    Change int16_t and uint8_t to __s16 and __u8

--- include/uapi/linux/sensor/range_sensor.h
+++ include/uapi/linux/sensor/range_sensor.h
@@ -3,12 +3,14 @@
 #ifndef LINUX_RANGE_SENSOR_H
 #define LINUX_RANGE_SENSOR_H
 
+#include <linux/types.h>
+
 #define NUMBER_OF_ZONE   64
 #define NUMBER_OF_TARGET  2
 
 struct range_sensor_data_t {
-	int16_t range_data[NUMBER_OF_ZONE][NUMBER_OF_TARGET];
-	uint8_t status[NUMBER_OF_ZONE][NUMBER_OF_TARGET];
+	__s16 range_data[NUMBER_OF_ZONE][NUMBER_OF_TARGET];
+	__u8 status[NUMBER_OF_ZONE][NUMBER_OF_TARGET];
 };
 
 #endif
